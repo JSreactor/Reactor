8f7a4a08c11d83713da33bd8512db67bdcb4b9d3
unknown <mlipovskiy@susachev.tqm>
2015-07-30 18:04:59 +0300

    reload

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('metronic/onepage2', { title: 'Express', what: 'best', who: 'me' });
-});
-
-module.exports = router;
